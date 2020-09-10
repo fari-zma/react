a37f4fdea5f207c63a37401d0aff9730b2ba92fe
fari-zma <ayaanthesuperstar@gmail.com>
2020-09-10 13:33:12 +0530

    update

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -33,7 +33,9 @@ function Header() {
       <div className="header_nav">
         <Link to={!user && "/login"}>
           <div onClick={handleAuthentication} className="header_option">
-            <span className="header_optionLineOne">Hello Guest</span>
+            <span className="header_optionLineOne">
+              {user ? user.email : "Hello Guest"}
+            </span>
             <span className="header_optionLineTwo">
               {user ? "Sign Out" : "Sign In"}
             </span>
